8a3154bd28b728614ef969bc1512be6df4abbff7
Vasiliy Kurganov <wkurganow@gmail.com>
2024-12-29 13:41:47 +0700

    Steps 5-6

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const type = document.querySelector('#type');
 const title = document.querySelector('#title');
 const value = document.querySelector('#value');
 
+const incomesList = document.querySelector('#incomes-list');
+const expensesList = document.querySelector('#expenses-list');
 // Actions
 
 form.addEventListener('submit', (e) => {
@@ -34,5 +36,35 @@ form.addEventListener('submit', (e) => {
         value: value.value,
     }
 
+    // Добавляем запись в данные(пушим в массив)
     budget.push(record)
+    // Отображаем доход на странице
+    if (record.type === 'inc'){
+        const html = `  <li data-id="${record.id}" class="budget-list__item item item--income">
+            <div class="item__title">${record.title}</div>
+            <div class="item__right">
+              <div class="item__amount">+ ${record.value}</div>
+              <button class="item__remove">
+                <img src="./img/circle-green.svg" alt="delete" />
+              </button>
+            </div>
+          </li>`
+        incomesList.insertAdjacentHTML('afterbegin', html);
+
+    }
+    // Отображаем расход на странице
+    if(record.type === 'exp'){
+        const html = `<li data-id="${record.id}" class="budget-list__item item item--expense">
+            <div class="item__title">${record.title}</div>
+            <div class="item__right">
+              <div class="item__amount">- ${record.value}</div>
+              <button class="item__remove">
+                <img src="./img/circle-red.svg" alt="delete" />
+              </button>
+            </div>
+          </li>`;
+        expensesList.insertAdjacentHTML('afterbegin', html);
+
+    }
+
 })
