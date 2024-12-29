1b16ef78ac4e7ecbe11fe3c4e1586693d4aad3fb
Vasiliy Kurganov <wkurganow@gmail.com>
2024-12-29 12:52:29 +0700

    Steps 1-2-3-4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log('Hello, world!)')
+// DATA
+const budget = [];
+
+// DOM
+const form = document.querySelector('#form');
+
+const type = document.querySelector('#type');
+const title = document.querySelector('#title');
+const value = document.querySelector('#value');
+
+// Actions
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // Расчет id
+    let id = 1;
+    if (budget.length > 0) {
+        // Последний элемент в массиве
+        const lastElement = budget[budget.length - 1];
+        // Получить ID последнего
+        const lastElId = lastElement.id;
+        // Сформировать новый id = старый + 1
+        id = lastElId + 1;
+
+    }
+
+
+    //Формируем запись
+    const record = {
+        id: 1,
+        type: type.value,
+        title: title.value,
+        value: value.value,
+    }
+
+    budget.push(record)
+})
